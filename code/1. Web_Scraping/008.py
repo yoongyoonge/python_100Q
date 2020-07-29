b4528c8613ce3680df61aa86096609819e5dcc6b
yoongyoonge <namyunjin0912@gmail.com>
2020-07-29 16:00:26 +0900

    2020.07.29

---
+++ code/1. Web_Scraping/008.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Seoul_Metropolitan_Subway"
+resp = requests.get(url)
+html_src = resp.text
+soup = BeautifulSoup(html_src, "html.parser")
+
+subway_image = soup.select('#mw-content-text > div > table:nth-child(3) > tbody > tr:nth-child(2) > td > a > img') # return : python list
+# ctrl + v : #mw-content-text > div > table:nth-child(3) > tbody > tr:nth-child(2) > td > a > img
+
+print(subway_image)
+print("\n")
+
+print(subway_image[0]) # html 요소 부분만을 추출
+print("\n")
+
+subway_image2 = soup.select("tr > td > a > img")
+print(subway_image2[1])
